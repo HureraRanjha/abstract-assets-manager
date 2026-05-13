a4db5c7fd6095ee2d223f9ab6af8c571adba7537
HureraRanjha <hureraranjha9@gmail.com>
2026-05-13 09:49:45 -0400

    Completed Exercise need to improve main

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
-public class Main {
+import java.util.ArrayList;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Asset> assets = new ArrayList<>();
+
+        Asset house1 = new House("This is my original house", "02/20/2025", 1_000_000, "105 village east", 2, 3400, 4000);
+//        Asset house2 = new House("This is VACCTION HOUSE")
+    }
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,5 +1,40 @@
 package com.pluralsight;
 
-public class Vehicle
+import java.time.LocalDate;
+
+public class Vehicle extends Asset
 {
+    String makeModel;
+    int year;
+    int odometer;
+
+    public Vehicle(String description, String dateAcquired, double originalCost, String makeModel, int year, int odometer)
+    {
+        super(description, dateAcquired, originalCost);
+        this.makeModel = makeModel;
+        this.year = year;
+        this.odometer = odometer;
+    }
+
+    @Override
+    public double getValue()
+    {
+        int currentYear = LocalDate.now().getYear();
+
+        int calcYear = currentYear - year;
+        double reducedCostPercent = 0;
+        double reducedCost = 0;
+        if(calcYear >= 0 && calcYear <= 3)  reducedCostPercent = 0.03;
+        else if (calcYear < 7) reducedCostPercent = 0.06;
+        else if (calcYear < 11) reducedCostPercent = 0.08;
+        else reducedCost = 1000;
+
+        if(odometer > 100_000 && !(makeModel.contains("honda") || makeModel.contains("toyota")))
+        {
+            reducedCostPercent += 0.25;
+        }
+
+        return originalCost * (1 - reducedCostPercent) - reducedCost;
+    }
+
 }
